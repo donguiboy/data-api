b9f9b4ffe8eb16b7a16b42a452d08655e6200361
gonzalo lara <gonzaloflara@gmail.com>
2023-02-26 22:58:57 -0300

    solved some issues with style

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+# pylint: disable=missing-module-docstring
 
 import sys
 import requests
@@ -6,6 +7,10 @@ BASE_URI = "https://weather.lewagon.com"
 
 
 def search_city(query):
+    '''
+    Look for a given city. If multiple options are returned, have the user choose between them.
+    Return one city (or None)
+    '''
     url = f'https://weather.lewagon.com/geo/1.0/direct?q={query}&limit=5'
     response = requests.get(url).json()
     if len(response) == 1:
@@ -20,6 +25,7 @@ def search_city(query):
     return response[final_answer]
 
 def weather_forecast(lat, lon):
+    '''Return a 5-day weather forecast for the city, given its latitude and longitude.'''
     url = f"https://weather.lewagon.com/data/2.5/forecast?lat={lat}&lon={lon}"
     response = requests.get(url).json()
     for i in range(0,len(response["list"]),8):
@@ -28,6 +34,7 @@ def weather_forecast(lat, lon):
         return forecasts
 
 def main():
+    '''Ask user for a city and display weather forecast'''
     query = input("City?\n> ")
     city = search_city(query)
     weather_forecast(city["lat"],city["lon"])
