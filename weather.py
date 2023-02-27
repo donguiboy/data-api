4a377d5e6b997c7ab07ff227f3285a09b217bfa4
gonzalo lara <gonzaloflara@gmail.com>
2023-02-26 21:02:58 -0300

    challenge done with every test passing and 10/10 style

--- weather.py
+++ weather.py
@@ -1,30 +1,36 @@
-# pylint: disable=missing-module-docstring
 
 import sys
-import urllib.parse
 import requests
 
 BASE_URI = "https://weather.lewagon.com"
 
 
 def search_city(query):
-    '''
-    Look for a given city. If multiple options are returned, have the user choose between them.
-    Return one city (or None)
-    '''
-    pass  # YOUR CODE HERE
+    url = f'https://weather.lewagon.com/geo/1.0/direct?q={query}&limit=5'
+    response = requests.get(url).json()
+    if len(response) == 1:
+        city = response[0]
+        return city
+    if len(response) == 0:
+        print("The city doesn't exist")
+        return None
+    for index, value in enumerate(response):
+        print (f"{index+1}. {value['name']}, {value['country']}")
+    final_answer = int(input("Multiple matches found, which city did you mean?\n")) -1
+    return response[final_answer]
 
 def weather_forecast(lat, lon):
-    '''Return a 5-day weather forecast for the city, given its latitude and longitude.'''
-    pass  # YOUR CODE HERE
+    url = f"https://weather.lewagon.com/data/2.5/forecast?lat={lat}&lon={lon}"
+    response = requests.get(url).json()
+    for i in range(0,len(response["list"]),8):
+        forecasts = []
+        forecasts.append(response["list"][i])
+        return forecasts
 
 def main():
-    '''Ask user for a city and display weather forecast'''
     query = input("City?\n> ")
     city = search_city(query)
-
-    # TODO: Display weather forecast for a given city
-    pass  # YOUR CODE HERE
+    weather_forecast(city["lat"],city["lon"])
 
 if __name__ == '__main__':
     try:
